97f958ef6caa77cb175fbd76ba9c21eab5847187
tatjama <tanja120a@gmail.com>
2020-04-07 09:01:13 +0200

    Refactoring js

--- js/openMenu.js
+++ js/openMenu.js
@@ -1,3 +1,9 @@
 function openMenu(){
     
+        document.querySelector('.nav-hamburger').style.display = 'none';
+        document.querySelector('.nav-close').style.display = 'initial';
+        document.querySelector('.nav-menu').style.display = 'initial';
+        document.querySelector('.section-one-svg').style.visibility = 'hidden';
+        document.querySelector('.nav-bg-menu').style.display = 'initial';
+      
 }
