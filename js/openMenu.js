27da29657f2818d6d463700d9400563e8e88ffa8
tatjama <tanja120a@gmail.com>
2020-04-08 06:27:10 +0200

    Chalenge done

--- js/openMenu.js
+++ js/openMenu.js
@@ -1,9 +1,14 @@
-function openMenu(){
-    
+function openMenu(){    
         document.querySelector('.nav-hamburger').style.display = 'none';
         document.querySelector('.nav-close').style.display = 'initial';
         document.querySelector('.nav-menu').style.display = 'initial';
         document.querySelector('.section-one-svg').style.visibility = 'hidden';
-        document.querySelector('.nav-bg-menu').style.display = 'initial';
-      
+        document.querySelector('.nav-bg-menu').style.display = 'initial';      
 }
+function closeMenu(){
+        document.querySelector('.nav-hamburger').style.display = "initial";
+        document.querySelector('.nav-close').style.display = 'none';
+        document.querySelector('.nav-bg-menu').style.display = 'none';
+        document.querySelector('.nav-menu').style.display = 'none';
+        document.querySelector('.section-one-svg').style.visibility = 'visible';
+      }
