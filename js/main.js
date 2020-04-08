ee7d4b4aa9668c8fb632e07ec8b9f87bd70e0f15
tatjama <tanja120a@gmail.com>
2020-04-08 18:20:01 +0200

    slider mobile responsive DONE

--- js/main.js
+++ js/main.js
@@ -11,4 +11,22 @@ function closeMenu(){
         document.querySelector('.nav-bg-menu').style.display = 'none';
         document.querySelector('.nav-menu').style.display = 'none';
         document.querySelector('.section-one-svg').style.visibility = 'visible';
-      }
+}
+function currentSlide(n){         
+        document.querySelector('.slider').style.left =185-n*375 +'px';          
+        var x = document.querySelector('.slider-dots').querySelectorAll('.dot');
+        for(i = 0; i<x.length; i++){
+          x[i].setAttribute("class", 'dot');
+        }          
+        x[1+n].setAttribute("class", " dot active");
+}
+function checkEMail(){       
+    
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
+         {    
+           document.getElementById('form-message').innerHTML= "ok";
+           return (true)    
+         }
+            document.getElementById('form-message').innerHTML = "Please insert a valid email";
+           return (false)  
+         }
